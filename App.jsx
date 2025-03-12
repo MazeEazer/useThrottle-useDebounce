4893d9589903a0992fcf9fa7eea1faac9ec2969f
MazeEazer <127978725+MazeEazer@users.noreply.github.com>
2025-03-12 20:32:17 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,11 @@
-/* import { useState } from "react"
+
+import React, { useState } from "react"
+import useThrottle from "./assets/useThrottle"
 import useDebounce from "./assets/useDebounce"
+
 function App() {
   const [inputValue, setInputValue] = useState("")
+  const throttledValue = useThrottle(inputValue, 1000) // Задержка 1000 мс
   const debouncedValue = useDebounce(inputValue, 500) // Задержка 500 мс
 
   return (
@@ -12,29 +16,8 @@ function App() {
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Введите текст"
       />
-      <p>Текст без задержки: {inputValue}</p>
-      <p>Текст с debounce: {debouncedValue}</p>
-    </div>
-  )
-}
+            <p>Текст с debounce: {debouncedValue}</p>
 
-export default App
- */
-import React, { useState } from "react"
-import useThrottle from "./assets/useThrottle"
-
-function App() {
-  const [inputValue, setInputValue] = useState("")
-  const throttledValue = useThrottle(inputValue, 1000) // Задержка 1000 мс
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Введите текст"
-      />
       <p>Текст без задержки: {inputValue}</p>
       <p>Текст с throttle: {throttledValue}</p>
     </div>
